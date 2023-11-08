60180267ce59fafb44e9cb4ffeceb18bdedb3b20
alvianpradentra <alvianpradentra3220@gmail.com>
2023-11-08 15:41:40 +0700

    push file postgre_connection.py

---
+++ playground.py
@@ -0,0 +1,18 @@
+# TUPPLE
+# my_tuples = (1, "Apple", 3.14)
+# # my_tuples[0] = 2
+# # print(my_tuples)
+
+# LIST
+# my_list = [1, 2, 3, 4, "String"]
+# my_list[-1] = 9999
+# print(my_list)
+
+# DICTIONARY
+# my_dict = {
+#  "nama": "alvian",
+#  "gender": "L",
+#  "no_telp": "621313"
+# }
+# my_dict['is_active'] = True 
+# print(my_dict)
